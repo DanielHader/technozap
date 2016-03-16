ded83301e866ecaa92d73a2f6db4b66a4b6d6a44
josephz885 <josephz885@gmail.com>
2016-03-16 17:03:52 -0500

    example usage of createGroup.php in the index page

--- createGroup_example.php
+++ createGroup_example.php
@@ -3,15 +3,13 @@
 	<h1>TechnoZap</h1>
   
 	<body>
-
-  	<hr><br>
-    <form action="createGroup.php" method="POST">
-      	Group Name:<input type="text" name="groupName"><br>
-      	Group Description:<input type="text" name="groupDesc"><br>
-    	<input type="submit" value="Create group">
-    </form>
-	<hr><br>
-
+  		<hr><br>
+	    <form action="createGroup.php" method="POST">
+	     	Group Name:<input type="text" name="groupName"><br>
+	      	Group Description:<input type="text" name="groupDesc"><br>
+	    	<input type="submit" value="Create group">
+	    </form>
+		<hr><br>
 	</body>
 
 </html>
