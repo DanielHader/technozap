accb8186bae44769be043ecd96baa36abf11db33
Joseph Zhang <josephz885@gmail.com>
2016-03-17 15:04:58 -0500

    updated

--- technozap.sql
+++ technozap.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 16, 2016 at 04:55 PM
+-- Generation Time: Mar 17, 2016 at 03:04 PM
 -- Server version: 5.6.28-0ubuntu0.15.10.1
 -- PHP Version: 5.6.11-1ubuntu3.1
 
@@ -30,15 +30,33 @@ CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `groups`
 --
 
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
-(1, 'Test', 'A group made to test functionalities.'),
-(2, 'Test 2', 'Another group to test a functionality.');
+(1, 'John Cena Fan Club', 'testingoaigisodfiosadfhnoaiw');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uglink`
+--
+
+CREATE TABLE IF NOT EXISTS `uglink` (
+  `linkid` int(11) NOT NULL,
+  `groupid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `uglink`
+--
+
+INSERT INTO `uglink` (`linkid`, `groupid`, `userid`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -53,7 +71,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `first name` text NOT NULL,
   `last name` text NOT NULL,
   `email` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first name`, `last name`, `email`) VALUES
+(1, 'john123', 'cena123', 'John', 'Cena', 'jcena@uark.edu');
 
 --
 -- Indexes for dumped tables
@@ -67,6 +92,12 @@ ALTER TABLE `groups`
   ADD UNIQUE KEY `id` (`id`),
   ADD KEY `id_2` (`id`),
   ADD KEY `id_3` (`id`);
+
+--
+-- Indexes for table `uglink`
+--
+ALTER TABLE `uglink`
+  ADD PRIMARY KEY (`linkid`);
 
 --
 -- Indexes for table `users`
@@ -83,12 +114,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `uglink`
+--
+ALTER TABLE `uglink`
+  MODIFY `linkid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
