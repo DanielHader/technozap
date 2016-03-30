da259caaae725a6727375c9349a19f7732422e51
Joseph Zhang <josephz885@gmail.com>
2016-03-30 11:20:39 -0500

    can no longer join a group the user is already in

--- util/joinGroup.php
+++ util/joinGroup.php
@@ -9,19 +9,23 @@ function joinGroup($conn, $group_select, $userId, &$message) {
 	foreach ($group_select as $group) {
 		cleanStrings($conn, $group);
 
-		// Check if a group already exists with the same name
+		// Check if the group exists
 		if ($result = mysqli_query($conn, "SELECT id FROM groups WHERE `name` LIKE '$group'")) {
 			if (mysqli_num_rows($result) > 0) {
 				$groupId = mysqli_fetch_array($result)["id"];
 
-				if (!mysqli_query($conn, "INSERT INTO uglink (linkid, groupid, userid) VALUES (null, '$groupId', '$userId')")) {
-					$message .= "<p>An error occurred in joining the group: ".mysqli_error($conn)."</p>";
-				}
-				else
-					$message .= "<p>Joined group '$group'!</p>";
+				// Checks if the user is already a part of this group
+				$result = mysqli_query($conn, "SELECT * FROM uglink WHERE `groupid` = '$groupId', `userid` = '$userId'");
+				if (mysqli_num_rows($result) > 0) {
+					if (!mysqli_query($conn, "INSERT INTO uglink (linkid, groupid, userid) VALUES (null, '$groupId', '$userId')"))
+						$message .= "<p>An error occurred in joining the group: ".mysqli_error($conn)."</p>";
+					else
+						$message .= "<p>Joined group '$group'!</p>";
+				} else
+					$message .= "<p>You are already a part of this group.</p>";
 			} else
 				$message .= "<p>There is no group called '$groupName'!<p>";
-
+				
 			mysqli_free_result($result);
 		}
 	}
