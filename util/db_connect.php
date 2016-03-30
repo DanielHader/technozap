356ba404952199d5f3d8dbbac8f8137e78f3eedc
Joseph Zhang <josephz885@gmail.com>
2016-03-30 11:21:26 -0500

    removed unreachable code after return statement

--- util/db_connect.php
+++ util/db_connect.php
@@ -1,18 +1,14 @@
 <?php
+	function connect() {
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$database = "technozap";
 
-function connect() {
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "technozap";
+		return mysqli_connect($servername, $username, $password, $database);
+	}
 
-	return mysqli_connect($servername, $username, $password, $database);
-
-	if (!$conn)
-		die("Could not connect to MySQL server: ".mysqli_connect_error());
-}
-
-function close($conn) {
-	mysqli_close($conn);
-}
+	function close($conn) {
+		mysqli_close($conn);
+	}
 ?>
