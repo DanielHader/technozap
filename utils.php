8dfdc64c094608d96f8dbf3193d1e159a9c81034
DanielHader <dhader96@gmail.com>
2016-04-19 23:10:36 -0500

    Revert "fixed logout"
    This reverts commit efaf279ec8da638c754cf84697e4f10e14fcec51.

--- utils.php
+++ utils.php
@@ -19,7 +19,7 @@
 	    session_write_close();
 	    setcookie(session_name(),'',0,'/');
 	    session_regenerate_id(true);
-	    header("Location: ../");
+	    header("Location: ../TechnoZap/");
 	}
 
 	function doRegister($conn, $username, $password, $fname, $lname, $email, &$register_notify) {
